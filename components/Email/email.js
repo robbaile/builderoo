a6b76741df38394304657c2ebadd54decab4cc16
Rob Baile <robert.baile@cazoo.co.uk>
2021-03-12 12:04:24 +0000

    add catch

--- components/Email/email.js
+++ components/Email/email.js
@@ -2,14 +2,26 @@ import { useRouter } from 'next/router';
 import Axios from 'axios';
 import formStyles from '../../styles/components/form.module.scss';
 import styles from './email.module.scss';
+import { useState } from 'react';
 
 export default function Email() {
     const router = useRouter();
-    const handleSubmit = (e) => {
+    const [isEmailError, setIsEmailError] = useState(false);
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const email = e.target.elements.email.value;
-        Axios.post('https://toolshed-email.herokuapp.com/api/email/new', {email});
-        router.push('/success');
+        try {
+            const response = await Axios.post('http://localhost:5000/api/email/new', {email});
+            if(!response.data.success) {
+                setIsEmailError(true);
+                return;
+            }
+
+            router.push('/success');
+        } catch (error) {
+            setIsEmailError(true) 
+            return;
+        }
     }
     return (
         <div className={styles.container}>      
@@ -21,6 +33,9 @@ export default function Email() {
                         <input id='Form-Email' className={formStyles.formInput} placeholder="e.g. johnsmith@gmail.com" type="email" name="email" />
                         <button className={formStyles.formButton} type="submit">Sign up</button>
                     </div>
+                    {isEmailError && (
+                        <p>We cannot register you right now</p>
+                    )}
                 </div>
             </form>
         </div>
